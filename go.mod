f3c4f35c782fc369588a540f9a1dbb6b08ca0136
wato787 <wato787@icloud.com>
2025-04-24 22:06:45 +0900

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wato787/atcoder_go_go
+
+go 1.20.6
---
+++ template.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+    sc.Scan()
+    i, _ := strconv.Atoi(sc.Text())
+    return i
+}
+
+func nextString() string {
+    sc.Scan()
+    return sc.Text()
+}
+
+func init() {
+    sc.Split(bufio.ScanWords)
+}
+
+func main() {
+    // ここに解答コードを書く
+}
